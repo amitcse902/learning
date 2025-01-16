05724626708f2d4f3d76f2741e79d30db3bb6be0
amitcse902 <43449793+amitcse902@users.noreply.github.com>
2025-01-16 22:54:41 +0530

    Update JS Problems.js

--- JS Problems.js
+++ JS Problems.js
@@ -1,12 +1,12 @@
 // Generate Random Password for provided length
 const generatePwd = (len)=>{
-    const albhabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     const number = '0123456789';
     const symbol = '!@#$%^&*';
     let randomPwd=''
     let allcharData = albhabets+number+symbol;
     
-     randomPwd += albhabets[Math.floor(Math.random()*albhabets.length)];
+     randomPwd += alphabets[Math.floor(Math.random()*alphabets.length)];
      randomPwd += number[Math.floor(Math.random()*number.length)];
      randomPwd += symbol[Math.floor(Math.random()*symbol.length)];
     
@@ -16,3 +16,21 @@ const generatePwd = (len)=>{
     return randomPwd;
 }
 console.log(generatePwd(8))
+
+//Sort the array by putting 0 in last 
+const arr = [0, 1, 111, 112, 21, 9,0, 8, 78]
+
+function sortArr(arr) {
+    const newList = [...arr];
+    const filterNew = arr.filter((item)=>item ===0)
+    const filterNewNonZ = arr.filter((item)=>item!==0)
+    const filteredSort = filterNewNonZ.sort((a,b)=>a-b)
+    const  newArr = [...filteredSort,...filterNew]
+    
+    return function sortArr(){
+        return newArr;
+    }
+}
+
+const sArr = sortArr(arr);
+console.log(sArr());
