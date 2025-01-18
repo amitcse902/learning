308872b883f8f72b9abf78ff8781523365a11f38
amitcse902 <43449793+amitcse902@users.noreply.github.com>
2025-01-18 22:53:36 +0530

    Update Polyfill.js

--- Polyfill.js
+++ Polyfill.js
@@ -9,5 +9,22 @@ Array.prototype.myMap= function (callbackFn){
 }
  
 const arr = [1,2,3]
-const arrMod = arr.myMap((item)=>item*2)
-console.log(arrMod)
+const arrData = arr.myMap((item)=>item*2)
+console.log(arrData)
+
+// Polyfill for reduce 
+Array.prototype.myReduce = function(reducer, initalValue) {
+  const araay = this;
+  let result = araay[0];
+  let init = 1;
+  if (typeof initalValue !== "undefined") {
+    result = initalValue;
+    init = 0;
+  }
+  for (let i = init; i < araay.length; i++) {
+    result = reducer(result, araay[i]);
+  }
+  return result;
+}
+const reduceSumCustom = list.myReduce(reducer, 1);
+console.log(reduceSumCustom);
