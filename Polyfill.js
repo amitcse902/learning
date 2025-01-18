e37414d64b2be5ea04abbd4431a0363691d9a337
amitcse902 <43449793+amitcse902@users.noreply.github.com>
2025-01-18 22:57:37 +0530

    Update Polyfill.js

--- Polyfill.js
+++ Polyfill.js
@@ -28,3 +28,21 @@ Array.prototype.myReduce = function(reducer, initalValue) {
 }
 const reduceSumCustom = list.myReduce(reducer, 1);
 console.log(reduceSumCustom);
+// Polyfill Promise All
+Promise.myAll = function (promiseList) {
+    const result = [];
+    promiseList.forEach(promise => {
+            promise()
+            .then(data => {
+                result.push(data)
+            })
+            .catch(err => {
+                return Promise.reject(err)
+            })
+    })
+    
+    if (promiseList.length === result.length) {
+        return Promise.resolve(result)
+    }
+}
+
