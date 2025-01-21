404f9ccd4d1d985259fab8e51dd01a00765775fc
amitcse902 <43449793+amitcse902@users.noreply.github.com>
2025-01-21 23:51:43 +0530

    Update Polyfill.js

--- Polyfill.js
+++ Polyfill.js
@@ -1,15 +1,17 @@
  // polyfill of map 
-Array.prototype.myMap= function (callbackFn){
-    const newList = [];
-    for(let i=0; i < this.length ;i++){
-        const val = callbackFn(this[i])
-        newList.push(val)
-    }
-    return newList;
-}
- 
+Array.prototype.customMap = function (callbackFn) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    const val = callbackFn(this[i])
+    result.push(val);
+  }
+  return result;
+};
+
+
+
 const arr = [1,2,3]
-const arrData = arr.myMap((item)=>item*2)
+const arrData = arr.customMap((item)=>item*2)
 console.log(arrData)
 
 // Polyfill for reduce 
