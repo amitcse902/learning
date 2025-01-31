3416d1507c2f6b4fdf4b756e75c7d5eba2d2095f
amitcse902 <43449793+amitcse902@users.noreply.github.com>
2025-02-01 01:07:55 +0530

    Update JS Problems.js

--- JS Problems.js
+++ JS Problems.js
@@ -256,4 +256,21 @@ let inputText = "Welcome to HCLTech";
 let outputText = reverseEachWord(inputText);
 console.log(outputText);
 
+// second largest number
+const arr = [10, 20, 4, 99, 99, 45,67];
+ const findSecondLargest = (arr)=>{
+     let firstLargest = -1
+     let secondLargest = -1;
+     for(let num of arr){
+         //console.log(`"num: " ${num},"firstLargest: "${firstLargest},"secondLargest: "${secondLargest}`)
+         if(num>firstLargest){
+             secondLargest = firstLargest;
+             firstLargest = num
+         } else if(num>secondLargest && num <firstLargest){
+             secondLargest = num
+         }
+     }
+     return secondLargest;
+ }
+ console.log(findSecondLargest(arr))
 
