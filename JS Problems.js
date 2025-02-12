0fc67d7dac74d70e8ddb631f1b6e426a426488c3
Amit Vishwakarma <43449793+amitcse902@users.noreply.github.com>
2025-02-13 00:26:57 +0530

    Update JS Problems.js

--- JS Problems.js
+++ JS Problems.js
@@ -303,3 +303,45 @@ const uniqArr =(arr)=>{
     return uniqList
 }
 console.log(uniqArr(arr))
+
+
+const arr = [13, 14, 15, 16, 13, 14];
+//with filter output [15,16];
+
+const removeDuplicateFilter = (arr) => {
+  const filterdata = arr.filter((item) => {
+    return arr.indexOf(item) === arr.lastIndexOf(item);
+  });
+  return filterdata;
+};
+console.log(removeDuplicateFilter(arr));
+
+//with reduce
+const removeDuplicateReduce = (arr) => {
+  const reducedata = arr.reduce((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  }, {});
+  return arr.filter((item) => reducedata[item] === 1);
+};
+console.log(removeDuplicateReduce(arr));
+
+// normal
+const removeDuplicate = (arr) => {
+  const res = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof res[arr[i]] === "undefined") {
+      res[arr[i]] = 0;
+    }
+    res[arr[i]] += 1;
+  }
+  const finalArr = [];
+  for (let i in res) {
+    if (res[i] === 1) {
+      finalArr.push(i);
+    }
+  }
+  return finalArr;
+};
+console.log(removeDuplicate(arr));
+
